3c7b132564bf9aa7d0d503588011c527fa187ea0
tonymumu <tony5023@163.com>
2020-11-27 22:30:30 +0800

    修改了git的email和username，测试github是否同步commit信息。

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm just for test github')
+    print_hi('just for test github commit')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
