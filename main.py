00971bd7e1ace7a75a7ce0e5a20935c7769e1643
yangmu <tony5023@163.com>
2020-11-27 16:40:36 +0800

    为了测试github，修改了print内容。

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm ym')
+    print_hi('PyCharm just for test github')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
